8daca9ea7a79c8a016a586b6b78e8c6661ae4330
Tom Bourjala <tom.bourjala@gmail.com>
2024-05-06 17:12:47 +0200

    feat: Add HomeSatellite component

---
+++ UI/src/components/HomeSatellite.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="satellite-container">
+    <div class="stat-name">
+      <div :class="props.satellite.status + ' status'"></div>
+      <div class="name">{{ props.satellite.name }}</div>
+    </div>
+    <div class="class">{{ props.satellite.class }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Satellite } from '@/types/Satellite';
+
+interface Props {
+  satellite: Satellite;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style scoped>
+.satellite-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2px 6px;
+  gap: 67px;
+  width: 100%;
+  height: 18px;
+  background: var(--color-layer);
+  border-radius: 10px;
+  margin: 5px 0;
+}
+
+.stat-name {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+}
+
+.status {
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+}
+
+.status.online {
+  background-color: var(--color-enabled);
+}
+
+.status.offline {
+  background-color: var(--color-unknown);
+}
+
+.status.error {
+  background-color: var(--color-disabled);
+}
+
+.status.pending {
+  background-color: var(--color-pending);
+}
+
+.name {
+  font-family: 'Hack Nerd Font Mono', monospace;
+  font-size: 12px;
+  color: var(--color-text);
+}
+
+.class {
+  font-family: 'Inter', sans-serif;
+  font-style: italic;
+  font-size: 9px;
+  color: var(--color-text);
+}
+</style>
