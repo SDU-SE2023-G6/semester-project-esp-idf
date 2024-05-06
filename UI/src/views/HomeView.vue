f8b000fd9e03863e970a36838cb0c6c56fe64cf6
Tom Bourjala <tom.bourjala@gmail.com>
2024-05-06 17:13:06 +0200

    refactor: Update HomeView to render HomeArea components for each area

--- UI/src/views/HomeView.vue
+++ UI/src/views/HomeView.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
+import HomeArea from '@/components/HomeArea.vue';
+import type { Satellite } from '@/types/Satellite';
+import { useDataStore } from '@/stores/dataStore';
+import type { Area } from '@/types/Area';
+
+const dataStore = useDataStore();
+
+const areas: Area[] = dataStore.getAreas;
+
 </script>
 
 <template>
   <div>
     <h1>Dashboard</h1>
+    <div>
+      <HomeArea v-for="area in areas" :key="area.id" :area="area" />
+    </div>
   </div>
 </template>
