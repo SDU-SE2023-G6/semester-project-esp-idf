b04069e36119838cd99d2141e55dcec67bf476bf
xdHampus <16954508+xdHampus@users.noreply.github.com>
2024-05-10 22:13:07 +0200

    feat: compile ESP with libraries

--- ESP-OTA-MQTT/nix-build.sh
+++ ESP-OTA-MQTT/nix-build.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+folder_path="./main"
+
+# Find all .c files in the folder recursively and extract their base names
+c_files=$(find "$folder_path" -maxdepth 1 -name "*.c" -exec basename {} \; | sed 's/.*/"&"/' | tr '\n' ' ')
+
+# Construct the sed command to replace SRCS argument
+sed_command="/^idf_component_register/s/SRCS .*/SRCS $c_files/"
+
+# Run sed to modify the file
+sed -i "$sed_command" ./main/CMakeLists.txt
 
 nix --offline --experimental-features 'nix-command flakes' develop github:mirrexagon/nixpkgs-esp-dev#esp32-idf -c cmake .
-
 nix --offline --experimental-features 'nix-command flakes' develop github:mirrexagon/nixpkgs-esp-dev#esp32-idf -c make
