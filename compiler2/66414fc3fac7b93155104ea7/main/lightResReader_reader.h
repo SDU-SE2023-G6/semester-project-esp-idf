2ec91c32d84487d07db07e8f2b11bcacab57877c
xdHampus <16954508+xdHampus@users.noreply.github.com>
2024-05-13 01:44:28 +0200

    chore: delete old example dir

--- compiler2/66414fc3fac7b93155104ea7/main/lightResReader_reader.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIGHTRESREADER_READER_H
-#define LIGHTRESREADER_READER_H
-#include <stdint.h>
-
-extern const int LIGHTRESREADER_READER_INPUT_SIZE;
-extern const int LIGHTRESREADER_READER_OUTPUT_SIZE;
-double* lightResReader_ReaderFunction(int *pin, int count);
-
-#endif /* LIGHTRESREADER_READER_H */
--- compiler2/66414fc3fac7b93155104ea8/main/files/spiffs.h
+++
@@ -1,8 +0,0 @@
-#ifndef SPIFFS_H
-#define SPIFFS_H
-
-#include "esp_err.h"
-
-esp_err_t init_spiffs();
-
-#endif
