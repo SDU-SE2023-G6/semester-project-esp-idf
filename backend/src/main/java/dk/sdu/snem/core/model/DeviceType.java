09247ca4fe4b44bec77d7843a1299a2d32abe3fe
Tom Bourjala <tom.bourjala@gmail.com>
2024-05-22 11:19:03 +0200

    refactor: Remove lazy loading for DocumentReference in DeviceType model

--- backend/src/main/java/dk/sdu/snem/core/model/DeviceType.java
+++ backend/src/main/java/dk/sdu/snem/core/model/DeviceType.java
@@ -17,7 +17,7 @@ public class DeviceType {
   private @MongoId ObjectId id;
   @Indexed(unique = true)
   private @NotBlank String name;
-  @DocumentReference(lazy = true)
+  @DocumentReference()
   private @Nullable Binary binary;
   private boolean deprecated = false;
 }
