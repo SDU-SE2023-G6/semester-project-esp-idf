a34fd5bc526242ee7dfc186ad71d39afb6b74ace
Henrik Schwarz <henrik@schwrz.dk>
2024-05-29 23:49:10 +0200

    fixed the structure and compiling

--- ESP-OTA-MQTT/main/headers/sensor_utils.h
+++ ESP-OTA-MQTT/main/headers/sensor_utils.h
@@ -1,15 +1,15 @@
 #ifndef SENSOR_UTILS_H // Double header include guard
 #define SENSOR_UTILS_H
 
+#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include "mqtt_client.h"
 
 void log_sensor_data(const char* sensor_path, const char* data);
 
-
 void sensor_routine(void *pvParameter);
 
-void read_and_delete_file2(const char* sensor_path);
+void read_and_delete_file(esp_mqtt_client_handle_t client, const char* sensor_path);
 
 int64_t xx_time_get_time();
 
